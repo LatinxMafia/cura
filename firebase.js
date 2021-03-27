617de0a506d78b402d9c6ede4a63e0446ae04485
Daniel <bdcaldera@gmail.com>
2021-03-26 19:33:06 -0600

    firebase config

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  // CONFIG FILES
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+
+export { db, auth, firebase, storage }
